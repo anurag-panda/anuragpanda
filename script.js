1fa1eca25fa52447f0519a18c26350943d7eee9b
anurag-panda <anuragpanda72w@gmail.com>
2024-08-28 13:09:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function hamburg() {
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(0px)"
+}
+
+function cancel() {
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(-500px)"
+}
+
